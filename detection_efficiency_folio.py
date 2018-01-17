73c849da03594c2ecd02aec74d37a253ce2a0535
William Saunders <william.saunders01@gmail.com>
2018-01-16 23:22:17 -0500

    Now tring it with >20 mag and < 2+m50.

--- detection_efficiency_folio.py
+++ detection_efficiency_folio.py
@@ -130,20 +130,20 @@ for expnum in expnums[:10]:
         print optimized.message
    
     # second round of optimization with better range
-#    print '          ...2'
-#    sys.stdout.flush()
-#    coadds_exp_found = coadds_exp_found[coadds_exp_found >= (optimized.x[0]-2)]                      
-#    coadds_exp_found = coadds_exp_found[coadds_exp_found <= (optimized.x[0]+2)]                      
-#    coadds_exp_missed = coadds_exp_missed[coadds_exp_missed >= (optimized.x[0]-2)]
-#    coadds_exp_missed = coadds_exp_missed[coadds_exp_missed <= (optimized.x[0]+2)]
+    print '          ...2'
+    sys.stdout.flush()
+    coadds_exp_found = coadds_exp_found[coadds_exp_found >= 20]                  
+    coadds_exp_found = coadds_exp_found[coadds_exp_found <= (optimized.x[0]+2)]                      
+    coadds_exp_missed = coadds_exp_missed[coadds_exp_missed >= 20]
+    coadds_exp_missed = coadds_exp_missed[coadds_exp_missed <= (optimized.x[0]+2)]
 
-#    optimized = optimize.minimize(minusLogP, (24, 2, .95), method='Nelder-Mead', \
-#                                  args=(coadds_exp_found, coadds_exp_missed), tol=1e-4)
-#    if optimized.success:
-#        opt_params = optimized.x
-#        print opt_params
-#    else:
-#        print optimized.message
+    optimized = optimize.minimize(minusLogP, (24, 2, .95), method='Nelder-Mead', \
+                                  args=(coadds_exp_found, coadds_exp_missed), tol=1e-2)
+    if optimized.success:
+        opt_params = optimized.x
+        print opt_params
+    else:
+        print optimized.message
 
     
 #    f.write('%d, %s, %.2f, %.3f, %.4f, %d, %d \n'%(expnum,band,optimized.x[0],optimized.x[1],optimized.x[2],\
