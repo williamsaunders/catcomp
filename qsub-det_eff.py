3c8f387d1379b7d8a234e554544341098146ff00
William Saunders <william.saunders01@gmail.com>
2018-04-21 19:26:27 -0400

    Added separate program to make sphere points and split them up.  going to adapt p9population.py to fit

--- qsub-det_eff.py
+++ qsub-det_eff.py
@@ -2,15 +2,15 @@ import numpy as np
 import os
 import glob 
 
-zonelist = np.loadtxt('zones_done.txt')
+zonelist = np.loadtxt('zones_done2.txt')
 zones = np.array(zonelist)
-z1, z2, z3, z4, z5 = np.array_split(zones, 5)
-for z, zs in zip([z1,z2,z3,z4,z5], ['z1','z2','z3','z4','z5']):
+z1, z2, z3, z4, z5, z6, z7, z8, z9 = np.array_split(zones, 9)
+for z, zs in zip([z1,z2,z3,z4,z5,z6,z7,z8,z9], ['z1','z2','z3','z4','z5','z6','z7','z8','z9']):
     f = open(zs + '.txt', 'w')
     for line in z:
         f.write('%d \n'%line)
     f.close()
 
-for z in ['z1','z2','z3','z4','z5']:
+for z in ['z1','z2','z3','z4','z5','z6','z7','z8','z9']:
     cmd = 'qsub -l h_vmem=10G -l des -cwd -b y -N Detection%s python detection_efficiency_folio.py '%z[-1] + '/data3/garyb/tno/matcher ' + z + '.txt'
     os.system(cmd)
